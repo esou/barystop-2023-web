ab11e4e0512275208a18477c2a5fcced64946eb7
Eva Soulier <eva.soulier@zenchef.com>
2023-08-08 23:58:37 +0200

    better style for date picker

--- src/components/CustomDatePicker.tsx
+++ src/components/CustomDatePicker.tsx
@@ -1,6 +1,6 @@
 import { format, isBefore } from 'date-fns'
 import * as React from 'react'
-import { Flex, HStack, styled } from '../../styled-system/jsx'
+import { Flex, HStack, Square, styled } from '../../styled-system/jsx'
 
 interface Props {
     dateList: string[]
@@ -10,41 +10,45 @@ interface Props {
 
 const CustomDatePicker = ({ dateList, dateIdxSelected, setDateIdxSelected }: Props) => {
     return (
-        <HStack justifyContent={'space-around'} alignItems={'center'}>
+        <HStack justifyContent={'space-around'} alignItems={'center'} gap={0}>
             {dateList.map((itm, idx) => {
                 const selected = dateIdxSelected === idx
                 const disabled = isBefore(new Date(), new Date(itm))
                 return (
-                    <styled.button
-                        key={idx}
-                        cursor={disabled ? 'wait' : 'pointer'}
-                        onClick={() => setDateIdxSelected(idx)}
-                        disabled={disabled}
-                        bg={'white'}
-                        boxShadow={selected ? 'lg' : 'sm'}
-                        boxShadowColor={'black'}
-                        borderRadius={'sm'}
-                        width={20}
-                        marginY={2}>
-                        <Flex
-                            borderTopRadius={'sm'}
-                            bg={disabled ? 'black' : selected ? 'red.600' : 'red.800'}
-                            alignItems={'center'}
-                            justifyContent={'center'}
-                            padding={1}>
-                            <styled.span textTransform={'uppercase'} color="white" fontSize={'xs'}>
-                                {format(new Date(itm), 'MMM')}
-                            </styled.span>
-                        </Flex>
-                        <Flex
-                            borderBottomRadius={'sm'}
-                            bg={selected ? 'white' : 'stone.100'}
-                            alignItems={'center'}
-                            justifyContent={'center'}
-                            padding={1}>
-                            <styled.span>{format(new Date(itm), 'dd')}</styled.span>
-                        </Flex>
-                    </styled.button>
+                    <Square>
+                        <styled.button
+                            key={idx}
+                            cursor={disabled ? 'wait' : 'pointer'}
+                            onClick={() => setDateIdxSelected(idx)}
+                            disabled={disabled}
+                            bg={'white'}
+                            boxShadow={selected ? 'lg' : 'sm'}
+                            boxShadowColor={'black'}
+                            borderRadius={'sm'}
+                            width={'100%'}>
+                            <Flex
+                                borderTopRadius={'sm'}
+                                bg={disabled ? 'black' : selected ? 'red.600' : 'red.800'}
+                                alignItems={'center'}
+                                justifyContent={'center'}
+                                paddingX={1}>
+                                <styled.span
+                                    textTransform={'uppercase'}
+                                    color="white"
+                                    fontSize={'xs'}>
+                                    {format(new Date(itm), 'MMM')}
+                                </styled.span>
+                            </Flex>
+                            <Flex
+                                borderBottomRadius={'sm'}
+                                bg={selected ? 'white' : 'stone.100'}
+                                alignItems={'center'}
+                                justifyContent={'center'}
+                                paddingX={1}>
+                                <styled.span>{format(new Date(itm), 'dd')}</styled.span>
+                            </Flex>
+                        </styled.button>
+                    </Square>
                 )
             })}
         </HStack>
