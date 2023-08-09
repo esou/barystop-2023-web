d71509a66406c06ce69cbb6efd8d69b93b1a9dc7
Eva Soulier <eva.soulier@zenchef.com>
2023-08-09 20:35:42 +0200

    [Map] - Style marker

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,11 +1,13 @@
 import Card from './Card'
-import { Divider, Flex, Stack } from '../../styled-system/jsx'
+import { Circle, Divider, Flex, Stack } from '../../styled-system/jsx'
 import { MapContainer, Marker, Polyline, Popup, TileLayer } from 'react-leaflet'
 import { getSteps } from '../services/webservices'
 import { format, isBefore } from 'date-fns'
-import { LatLngBoundsExpression, LatLngExpression } from 'leaflet'
+import { DivIcon, LatLngBoundsExpression, LatLngExpression } from 'leaflet'
 import Title from './Title'
 import { useQuery } from 'react-query'
+import { renderToString } from 'react-dom/server'
+import { token } from '../../styled-system/tokens'
 
 const DEFAULT_ZOOM = 11
 const DEFAULT_LOC = { lat: 48.864716, lng: 2.349014 }
@@ -49,9 +51,26 @@ const MapComponent = () => {
                         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
-                    <Polyline positions={polyline} />
+                    <Polyline positions={polyline} color={token('colors.secondary')} />
                     {visibleSteps.map((step, idx) => (
-                        <Marker key={idx} position={{ lat: step.lat, lng: step.lon }}>
+                        <Marker
+                            key={idx}
+                            position={{ lat: step.lat, lng: step.lon }}
+                            icon={
+                                new DivIcon({
+                                    html: renderToString(
+                                        <Circle
+                                            bg={'primary'}
+                                            size={'20px'}
+                                            position="relative"
+                                            top={'-5px'}
+                                            left={'-5px'}
+                                            boxShadow={`2px 2px 0px token(colors.secondary)`}
+                                            border={`2px solid token(colors.secondary)`}
+                                        />
+                                    ),
+                                })
+                            }>
                             <Popup>
                                 <Stack fontFamily={'paris'} color={'secondary'}>
                                     <div>
