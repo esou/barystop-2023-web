02fa3042184312386ce9007842fc374049eca350
Eva Soulier <eva.soulier@zenchef.com>
2023-08-10 15:06:25 +0200

    add msg in console

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import routes from './routes'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { setDefaultOptions } from 'date-fns'
 import { fr } from 'date-fns/locale'
+import React from 'react'
+import { token } from '../styled-system/tokens'
 
 const queryClient = new QueryClient({
     defaultOptions: { queries: { staleTime: Infinity, refetchInterval: 60 * 60 * 1000 } },
@@ -10,6 +12,13 @@ const queryClient = new QueryClient({
 
 function App() {
     setDefaultOptions({ locale: fr })
+
+    React.useEffect(() => {
+        console.log(
+            '%cGros malins, pas de spoil SVP ;)',
+            `color: ${token('colors.primary')}; font-size: 50px;`
+        )
+    }, [])
 
     return (
         <QueryClientProvider client={queryClient}>
